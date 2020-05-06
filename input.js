26bbe2165b25812a5ca9a876be69887c54a4c11a
Ben S <shaw.j.benjamin@gmail.com>
2020-05-06 18:39:26 +0000

    moved input functions into input.js

---
+++ input.js
@@ -0,0 +1,26 @@
+const handleUserInput = function() {
+  const stdin = process.stdin;
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    } else {
+      process.stdout.write('.');
+    }
+  });
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+}
+
+
+let setupInputObj = {
+  setupInput
+};
+
+module.exports = setupInputObj;
